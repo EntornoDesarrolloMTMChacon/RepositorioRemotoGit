ff00a9168faf94825778831c561f33e6fc0a7f5b
TP412FAC <TP412FAC@DESKTOP-1SH9GRS>
2023-03-09 21:49:21 +0100

    prueba del dia

---
+++ ejemplo1/src/ejemplo1/ejemplo1.java
@@ -0,0 +1,12 @@
+package ejemplo1;
+
+public class ejemplo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("esta haciendo control de versiones");
+		
+
+	}
+
+}
