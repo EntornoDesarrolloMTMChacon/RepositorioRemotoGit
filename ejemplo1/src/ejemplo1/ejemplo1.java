d8014459fdc8863ed637cf47b8c0ccec09557cea
TP412FAC <TP412FAC@DESKTOP-1SH9GRS>
2023-03-09 21:54:33 +0100

    eree

--- ejemplo1/src/ejemplo1/ejemplo1.java
+++ ejemplo1/src/ejemplo1/ejemplo1.java
@@ -5,7 +5,7 @@ public class ejemplo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("esta haciendo control de versiones");
-		
+		System.out.println("esto es otra prueba");
 
 	}
 
