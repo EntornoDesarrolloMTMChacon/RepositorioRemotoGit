25590ab3b8de8e6c2bc16baf8bf746c76ade923f
TP412FAC <TP412FAC@DESKTOP-1SH9GRS>
2023-03-17 02:11:38 +0100

    prueba

---
+++ xpruebagit2/src/main/java/mavenproject3/xpruebagit2/Xpruebagit2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package mavenproject3.xpruebagit2;
+
+/**
+ *
+ * @author TP412FAC
+ */
+public class Xpruebagit2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
